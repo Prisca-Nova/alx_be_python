83dce79c121760f799618bfa51852622af103d1b
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-10-12 23:49:27 +0200

    the changes

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,30 +1,14 @@
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
+        self.year = year
 
-class EBook(Book):
-    def __init__(self, title, author, file_size):
-        super().__init__(title, author)
-        self.file_size = file_size
+    def __del__(self):
+        print(f"Deleting {self.title}")
 
-class PrintBook(Book):
-    def __init__(self, title, author, page_count):
-        super().__init__(title, author)
-        self.page_count = page_count
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
 
-class Library:
-    def __init__(self):
-        self.books = []
-
-    def add_book(self, book):
-        self.books.append(book)
-
-    def list_books(self):
-        for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
